1e3cf9fd1fb7ee7e0916afe26324a82910e35a12
TestingTesting198 <68979007+TestingTesting198@users.noreply.github.com>
2020-07-31 17:48:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function calculate(){
 
 function display(){
 	var pull = new XMLHttpRequest()
-	pull.open("GET", `https://scratchcors.herokuapp.com/https://api.scratch.mit.edu/studios/${sid}/`);
+	pull.open("GET", `https://cors-anywhere.herokuapp.com/https://api.scratch.mit.edu/studios/${sid}/`);
 	pull.send();
 		pull.onreadystatechange = function() {
 			if (pull.readyState === 4 && pull.status === 200) {
